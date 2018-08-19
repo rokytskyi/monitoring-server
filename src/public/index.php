08b35f67809d238ae762b02591640bd66e99dd94
Oleksandr Rokytskyi <rokytskyi@MacBook-Pro-Oleksandr.local>
2018-08-19 21:16:49 +0300

    Init slim application

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,22 @@
 <?php
-echo "Hello world";
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../../vendor/autoload.php';
+
+require '../../config.php';
+
+$app = new \Slim\App(['settings' => $config]);
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+$app->get('/', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write("Hello world");
+
+    return $response;
+});
+$app->run();
