e16a3531bbdb8c69b2d8870cb3bbc84cf61234fd
Oleksandr Rokytskyi <o.rokytskyi@spheremall.com>
2018-12-02 19:39:53 +0200

    Add diferent types of routes

--- src/public/index.php
+++ src/public/index.php
@@ -14,9 +14,46 @@ $app->get('/hello/{name}', function (Request $request, Response $response, array
 
     return $response;
 });
+
+$app->get('/slow', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write("Slow response");
+    sleep(10);
+    return $response;
+});
+
+$app->get('/very_slow', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write("Very slow response");
+    sleep(20);
+    return $response;
+});
+
+$app->get('/internal_error', function (Request $request, Response $response, array $args) {
+
+    return $response->withStatus(500, "Internal server error");
+});
+
+$app->get('/unauthorized', function (Request $request, Response $response, array $args) {
+
+    return $response->withStatus(401, "Unauthorized");
+});
+
+$app->get('/unauthorized_two', function (Request $request, Response $response, array $args) {
+
+    return $response->withStatus(401, "Unauthorized");
+});
+
 $app->get('/', function (Request $request, Response $response, array $args) {
     $response->getBody()->write("Hello world");
 
     return $response;
 });
+
+$app->post('/create_smth', function (Request $request, Response $response, array $args) {
+    return $response->withStatus(200, "Created");
+});
+
+$app->put('/save_smth', function (Request $request, Response $response, array $args) {
+    return $response->withStatus(200, "Saved");
+});
+
 $app->run();
